52036ef63d9f1abdd7acd191e827277d3e452a73
Carl Wiede <guardsmancarl@gmail.com>
2024-12-01 10:04:07 +0100

    Solve day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,9 +1,48 @@
-pub fn part1(_input: Vec<String>) -> String {
-    // Your Part 1 solution logic
-    "Part 1 not implemented yet".to_string()
+pub fn part1(input: Vec<String>) -> String {
+
+    let mut total_diff = 0;
+    let mut left_vals: Vec<i32> = vec![];
+    let mut right_vals: Vec<i32> = vec![];
+
+    for s in input.iter() {
+        let vals: Vec<&str> = s.split_whitespace().collect();
+        left_vals.push(vals.first().unwrap().parse::<i32>().unwrap());
+        right_vals.push(vals.last().unwrap().parse::<i32>().unwrap());
+    }
+
+    for _ in input.iter() {
+        if let (Some (&min_left), Some(&min_right)) = (left_vals.iter().min(), right_vals.iter().min()) {
+            total_diff += (min_left - min_right).abs();
+
+            if let Some(min_left_index) = left_vals.iter().position(|&x| x == min_left) {
+                left_vals.remove(min_left_index);
+            }
+            if let Some(min_right_index) = right_vals.iter().position(|&x| x == min_right) {
+                right_vals.remove(min_right_index);
+            }
+        }
+    }
+
+    format!("{}", total_diff)
 }
 
-pub fn part2(_input: Vec<String>) -> String {
-    // Your Part 2 solution logic
-    "Part 2 not implemented yet".to_string()
+use std::collections::HashMap;
+
+pub fn part2(input: Vec<String>) -> String {
+    
+    let mut similarity_score = 0;
+    let mut left_vals: Vec<i32> = vec![];
+    let mut right_vals: HashMap<i32, i32> = HashMap::new();
+
+    for s in input.iter() {
+        let vals: Vec<&str> = s.split_whitespace().collect();
+        left_vals.push(vals.first().unwrap().parse::<i32>().unwrap());
+        *right_vals.entry(vals.last().unwrap().parse::<i32>().unwrap()).or_insert(0) += 1;
+    }
+
+    while let Some(val) = left_vals.pop() {
+        similarity_score += val * right_vals.get(&val).unwrap_or(&0);
+    }
+
+    format!("{}", similarity_score)
 }
