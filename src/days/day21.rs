88d0e1e915a9957a2a1ef5560bd925dff65b8718
Carl Wiede <guardsmancarl@gmail.com>
2024-12-23 13:53:08 +0100

    Work in progress pppp

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -43,40 +43,40 @@ pub fn part1(input: Vec<String>) -> String
 
 pub fn part2(input: Vec<String>) -> String
 {
-//    let mut num_keypad: HashMap<char, Point> = HashMap::new();
-//    num_keypad.insert('A', (3, 2));
-//    num_keypad.insert('0', (3, 1));
-//    num_keypad.insert('1', (2, 0));
-//    num_keypad.insert('2', (2, 1));
-//    num_keypad.insert('3', (2, 2));
-//    num_keypad.insert('4', (1, 0));
-//    num_keypad.insert('5', (1, 1));
-//    num_keypad.insert('6', (1, 2));
-//    num_keypad.insert('7', (0, 0));
-//    num_keypad.insert('8', (0, 1));
-//    num_keypad.insert('9', (0, 2));
-//
-//    let mut dir_keypad: HashMap<char, Point> = HashMap::new();
-//    dir_keypad.insert('A', (0, 2));
-//    dir_keypad.insert('^', (0, 1));
-//    dir_keypad.insert('>', (1, 2));
-//    dir_keypad.insert('v', (1, 1));
-//    dir_keypad.insert('<', (1, 0));
+    let mut num_keypad: HashMap<char, Point> = HashMap::new();
+    num_keypad.insert('A', (3, 2));
+    num_keypad.insert('0', (3, 1));
+    num_keypad.insert('1', (2, 0));
+    num_keypad.insert('2', (2, 1));
+    num_keypad.insert('3', (2, 2));
+    num_keypad.insert('4', (1, 0));
+    num_keypad.insert('5', (1, 1));
+    num_keypad.insert('6', (1, 2));
+    num_keypad.insert('7', (0, 0));
+    num_keypad.insert('8', (0, 1));
+    num_keypad.insert('9', (0, 2));
+
+    let mut dir_keypad: HashMap<char, Point> = HashMap::new();
+    dir_keypad.insert('A', (0, 2));
+    dir_keypad.insert('^', (0, 1));
+    dir_keypad.insert('>', (1, 2));
+    dir_keypad.insert('v', (1, 1));
+    dir_keypad.insert('<', (1, 0));
 
     let mut total = 0;
 
-//    for line in input {
-//        let mut min_len = isize::MAX;
-//        for i in 0..1 {
-//            println!("{}", i);
-//            let mut seq = get_sequence(&line, &num_keypad, (3, 2), (3, 0));
-//            for _ in 0..25 {
-//                seq = get_sequence(&seq, &dir_keypad, (0, 2), (0, 0));
-//            }
-//            if (seq.len() as isize) < min_len { min_len = seq.len() as isize }
-//        }
-//        total += min_len * get_numeric_part(&line);
-//    }
+    for line in input {
+        let mut min_len = isize::MAX;
+        for i in 0..1 {
+            println!("{}", i);
+            let mut seq = get_sequence(&line, &num_keypad, (3, 2), (3, 0));
+            for _ in 0..25 {
+                seq = get_sequence(&seq, &dir_keypad, (0, 2), (0, 0));
+            }
+            if (seq.len() as isize) < min_len { min_len = seq.len() as isize }
+        }
+        total += min_len * get_numeric_part(&line);
+    }
 
     format!("{}", total)
 }
